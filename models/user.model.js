5a4afeb03f590308cd91d792cfaab6d3748a09df
Youssef Mohamed <youssef.mu.saber@gmail.com>
2026-01-16 11:04:28 +0200

    refactor: updated the user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,48 +1,32 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Name is required'],
+        required: [true, 'User Name is required'],
+        trim: true,
+        min: 4,
+        max: 64,
     },
     email: {
         type: String,
         required: [true, 'Email is required'],
-        unique: true,
+        unique: [true, 'User Email must be unique'],
         trim: true,
         lowercase: true,
-        match: [/^\S+@\S+\.\S+$/, "Please fill a valid email address"],
+        min: 8,
+        max: 255,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email address.']
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
-        minlength: [8, 'Password must be at least 8 characters long'],
-        select: false
-    },
-    currencyId: {
-        type: Number,
-    },
-    salary: {
-        type: Number,
-        default: 0
-    },
-    salaryDay: {
-        type: Number,
-        min: 1,
-        max: 31
-    },
-    currentBalance: {
-        type: Number,
-        default: 0
-    },
-    categories: {
-        type: [String],
-        default: []
-    },
-}, {
-    timestamps: true
-});
+        trim: true,
+        min: 8,
+        max: 32
+    }
+}, {timestamps: true});
 
-const User = mongoose.model('User', userSchema);
+const user = mongoose.model('User', userSchema);
 
-export default User;
+export default user;
