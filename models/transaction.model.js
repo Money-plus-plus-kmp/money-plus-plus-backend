4eb282a11015a2a5031c5545ba425d7d9681f60c
gehadepada <gehadepada@gmail.com>
2026-01-29 14:32:45 +0200

    fix:fix exporting of transaction model

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const transactionSchema = new mongoose.Schema({
+const transactionSchema = new mongoose.Schema({
     userId:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
@@ -40,3 +40,6 @@ export const transactionSchema = new mongoose.Schema({
     }
 
 },{timestamps:true})
+const Transaction = mongoose.model('Transaction', transactionSchemaSchema);
+
+export default Transaction;
