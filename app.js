7006a5bfaeea3696862350e61696d961b1851025
gehadepada <gehadepada@gmail.com>
2026-01-29 17:13:50 +0200

    fix:fix node environment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api/v1/transaction/' ,transactionRouter)
 
 app.use(errorMiddleware)
 
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV == 'development') {
     app.listen(PORT, async () => {
         console.log(`App listening on http://localhost:${PORT}`);
         await connectToDatabase();
