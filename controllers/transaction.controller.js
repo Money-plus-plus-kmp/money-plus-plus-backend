edd574bf8807a9363817a66eb89a78e1e3c57753
gehadepada <gehadepada@gmail.com>
2026-01-29 17:44:57 +0200

    feat:add get transaction details end point

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -4,16 +4,14 @@ import User from "../models/user.model.js";
 export const addTransaction= async(req ,res ,  next)=>{
 
     try{
-       
         const {userId , type , amount , category , currency , date , note} = req.body
          if (!amount || amount <= 0) {
             throwError(400, "Amount must be greater than zero")
         }
-        console.log("userId received:", userId);
 
-       const currentUser = await User.findById(userId);
-       console.log("currentUser:", currentUser);
+        const currentUser = await User.findById(userId);
         if (!currentUser) throwError(404, "User not found");
+
         (type =="income" ) ? currentUser.salary+=amount : currentUser.salary-=amount
         if (currentUser.balance < 0) {
             throwError(400, "Insufficient balance")
@@ -41,6 +39,20 @@ export const addTransaction= async(req ,res ,  next)=>{
 
     }
 };
-export const getTransactionDetails = async(req , res , next)=>{
+export const getTransactionDetailsById = async(req , res , next)=>{
+    try{
+    const {transactionId} = req.params;
+    if (!transactionId) throwError(400, "transactionId is required");
 
-}
+    const transaction = await Transaction.findById(transactionId)
+     if (!transaction) throwError(404, "Transaction not found");
+      res.status(200).json({
+            code: 200,
+            message: "Transaction details fetched successfully",
+            data: transaction
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addTransaction } from "../controllers/transaction.controller";
+import { addTransaction, getTransactionDetailsById } from "../controllers/transaction.controller.js";
 
 const transactionRouter = Router()
 
 transactionRouter.post('/addTransaction',addTransaction)
+transactionRouter.get('/getTransaction/:transactionId', getTransactionDetailsById)
 
 export default transactionRouter;
