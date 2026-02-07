d3ee422ff2d2f1f238888a7a96a7e4b9f6f2b70e
Youssef Mohamed <youssef.mu.saber@gmail.com>
2026-02-07 12:44:31 +0200

    implemented the reset password endpoint

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { signUp, login } from '../controllers/auth.controller.js';
+import { signUp, login, forgotPassword, resetPassword } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
 authRouter.post('/signup', signUp);
 authRouter.post('/login', login);
+authRouter.post("/forgot-password", forgotPassword);
+authRouter.post("/reset-password", resetPassword);
+
 
 export default authRouter;
