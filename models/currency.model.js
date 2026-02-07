2e61d02bff4234adb2dcc80a09dbd2c5f54d98c0
Youssef Mohamed <youssef.mu.saber@gmail.com>
2026-02-07 10:33:23 +0200

    created the currency model

---
+++ models/currency.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const currencySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    code: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+const Currency = mongoose.model("Currency", currencySchema);
+export default Currency;
