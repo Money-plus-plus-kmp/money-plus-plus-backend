2383219c9b1f9e06cb856a40eb10cd158e8cc206
gehadepada <gehadepada@gmail.com>
2026-02-26 00:19:22 +0200

    feat:add get categories route

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { addCategory } from "../controllers/category.controller.js";
+import { addCategory, getCategories } from "../controllers/category.controller.js";
 
 const categoryRouter = Router()
 categoryRouter.post('/add-category' , authenticate ,addCategory)
+categoryRouter.get('/get-categories' , authenticate ,getCategories)
+
 
 export default categoryRouter;
