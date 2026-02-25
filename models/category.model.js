62f3052a618dc2ab50de2fb641178e98fd4ac207
gehadepada <gehadepada@gmail.com>
2026-02-25 21:52:21 +0200

    feat:add category model

---
+++ models/category.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    userId:{
+         type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: [true],
+
+    },
+    name:{
+        type:String,
+        required: [true],
+    }
+},{ timestamps: true })
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+export default Category;
