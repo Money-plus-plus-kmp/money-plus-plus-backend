a39362b12330169c8d63ad420c9e7d92e6b6ff80
gehadepada <gehadepada@gmail.com>
2026-02-26 22:47:07 +0200

    fix:return just user category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,41 +1,40 @@
 import Category from "../models/category.model.js";
 import { throwError } from "../utils/errorHandle.js";
-export const addCategory= async(req , res , next)=>
-{
-    try{
-       const {name} = req.body
-       const currentUser = req.user
-       if (!currentUser) throwError(401, "User not found");
+export const addCategory = async (req, res, next) => {
+    try {
+        const { name } = req.body
+        const currentUser = req.user
+        if (!currentUser) throwError(401, "User not found");
         const category = await Category.create({
-                   userId: currentUser._id,
-                   name
+            userId: currentUser._id,
+            name
         })
-         res.status(201).json({
+        res.status(201).json({
             code: 201,
             message: 'Category Added succeefully',
             data: category
         });
     }
-    catch(error)
-    {
+    catch (error) {
         next(error);
     }
 
 };
-export const getCategories = async(req , res , next )=>
-{
-    try{
-        const categories = await Category.find({});
-        if (!categories || categories.length == 0 ) throwError(404, "No Categories found");
-                res.status(200).json({
-                    code: 200,
-                    message: "Categories Fetched successfully",
-                    data: categories
-                });
+export const getCategories = async (req, res, next) => {
+    try {
+        const currentUser = req.user
+        const categories = await Category.find({
+            userId: currentUser._id
+        });
+        if (!categories || categories.length == 0) throwError(404, "No Categories found");
+        res.status(200).json({
+            code: 200,
+            message: "Categories Fetched successfully",
+            data: categories
+        });
 
     }
-    catch(error)
-    {
+    catch (error) {
         next(error);
     }
 };
--- routes/category.route.js
+++ routes/category.route.js
@@ -3,8 +3,8 @@ import { authenticate } from "../middlewares/auth.middleware.js";
 import { addCategory, getCategories } from "../controllers/category.controller.js";
 
 const categoryRouter = Router()
-categoryRouter.post('/add-category' , authenticate ,addCategory)
-categoryRouter.get('/get-categories' , authenticate ,getCategories)
+categoryRouter.post('/add-category', authenticate, addCategory)
+categoryRouter.get('/get-categories', authenticate, getCategories)
 
 
 export default categoryRouter;
